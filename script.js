19aa3156cb17877c2a96870b9358f902247535da
mustapha <mustapha.najem.14@gmail.com>
2023-07-16 12:25:19 +0200

    test js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+
 const ratingNumber=document.querySelector(".rating-number");
+const number=document.querySelector(".rating-number h2")
 
 
-function clickNumber(){
-    if(ratingNumber.classList.contains("numberClick")){
+function clrChange(){
+    if(ratingNumber.classList.contains("rating-number")){
     
-    ratingNumber.classList.remove("numberClick");
+    ratingNumber.style.backgroundColor='orange';
+    number.style.color='white';
     }else{
-    ratingNumber.classList.add("numberClick")
+    ratingNumber.classList.toggle("rating-number");
     }
 }
-ratingNumber.addEventListener("click",clickNumber);
+ratingNumber.addEventListener("click",clrChange);
